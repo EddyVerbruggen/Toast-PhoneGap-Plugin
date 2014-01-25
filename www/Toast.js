37b3857673555877bf3f4d02ddebaaef53fd990a
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-01-25 13:40:53 +0100

    implemented Toast params from JS

--- www/Toast.js
+++ www/Toast.js
@@ -1,41 +1,41 @@
-cordova.define("nl.x-services.plugins.toast.Toast", function(require, exports, module) {function Toast() {
+function Toast() {
 }
 
-  Toast.prototype._show = function (message, duration, position, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Toast", "show", [message, duration, position]);
-  };
+Toast.prototype._show = function (message, duration, position, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "Toast", "show", [message, duration, position]);
+};
 
-  Toast.prototype.shortTop = function (message, successCallback, errorCallback) {
-    this._show(message, "short", "top", successCallback, errorCallback);
-  };
+Toast.prototype.shortTop = function (message, successCallback, errorCallback) {
+  this._show(message, "short", "top", successCallback, errorCallback);
+};
 
-  Toast.prototype.shortCenter = function (message, successCallback, errorCallback) {
-    this._show(message, "short", "center", successCallback, errorCallback);
-  };
+Toast.prototype.shortCenter = function (message, successCallback, errorCallback) {
+  this._show(message, "short", "center", successCallback, errorCallback);
+};
 
-  Toast.prototype.shortBottom = function (message, successCallback, errorCallback) {
-    this._show(message, "short", "bottom", successCallback, errorCallback);
-  };
+Toast.prototype.shortBottom = function (message, successCallback, errorCallback) {
+  this._show(message, "short", "bottom", successCallback, errorCallback);
+};
 
-  Toast.prototype.longTop = function (message, successCallback, errorCallback) {
-    this._show(message, "long", "top", successCallback, errorCallback);
-  };
+Toast.prototype.longTop = function (message, successCallback, errorCallback) {
+  this._show(message, "long", "top", successCallback, errorCallback);
+};
 
-  Toast.prototype.longCenter = function (message, successCallback, errorCallback) {
-    this._show(message, "long", "center", successCallback, errorCallback);
-  };
+Toast.prototype.longCenter = function (message, successCallback, errorCallback) {
+  this._show(message, "long", "center", successCallback, errorCallback);
+};
 
-  Toast.prototype.longBottom = function (message, successCallback, errorCallback) {
-    this._show(message, "long", "bottom", successCallback, errorCallback);
-  };
+Toast.prototype.longBottom = function (message, successCallback, errorCallback) {
+  this._show(message, "long", "bottom", successCallback, errorCallback);
+};
 
-  Toast.install = function () {
-    if (!window.plugins) {
-      window.plugins = {};
-    }
+Toast.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
 
-    window.plugins.toast = new Toast();
-    return window.plugins.toast;
-  };
+  window.plugins.toast = new Toast();
+  return window.plugins.toast;
+};
 
-  cordova.addConstructor(Toast.install);});
+cordova.addConstructor(Toast.install);
