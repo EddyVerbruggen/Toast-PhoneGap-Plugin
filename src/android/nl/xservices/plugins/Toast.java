cd6564784139610ba4844937ba503f9c1fe1d3fe
EddyVerbruggen <eddyverbruggen@gmail.com>
2015-01-13 16:04:58 +0100

    #23 Notifications while app is suspended

--- src/android/nl/xservices/plugins/Toast.java
+++ src/android/nl/xservices/plugins/Toast.java
@@ -26,6 +26,11 @@ public class Toast extends CordovaPlugin {
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     if (ACTION_SHOW_EVENT.equals(action)) {
 
+      if (webView.isPaused()) {
+        // suppress while paused
+        return true;
+      }
+
       final String message = args.getString(0);
       final String duration = args.getString(1);
       final String position = args.getString(2);
